7a58573d7919af2cf0eb749e1182489548aa4331
boyuz5 <40516119+boyuz5@users.noreply.github.com>
2018-10-12 20:43:02 -0400

    Update lib.php

--- web/lib.php
+++ web/lib.php
@@ -67,37 +67,37 @@ function videoCategories($apikey,$regionCode='HK'){
 
 function categorieslist($id){
    $data=array(
-    '1' => '电影和动画',
-    '2' => '汽车',
-    '10' => '音乐',
-    '15' => '宠物和动物',
-    '17' => '体育',
-    '18' => '短片',
-    '19' => '旅游和活动',
-    '20' => '游戏',
-    '21' => '视频博客',
-    '22' => '人物和博客',
-    '23' => '喜剧',
-    '24' => '娱乐',
-    '25' => '新闻和政治',
-    '26' => 'DIY 和生活百科',
-    '27' => '教育',
-    '28' => '科学和技术',
-    '30' => '电影',
-    '31' => '动漫/动画',
-    '32' => '动作/冒险',
-    '33' => '经典',
-    '34' => '喜剧',
-    '35' => '纪录片',
-    '36' => '剧情片',
-    '37' => '家庭片',
-    '38' => '外国',
-    '39' => '恐怖片',
-    '40' => '科幻/幻想',
-    '41' => '惊悚片',
-    '42' => '短片',
-    '43' => '节目',
-    '44' => '预告片'
+    '1' => 'Movies',
+    '2' => 'Cars',
+    '10' => 'Music',
+    '15' => 'Pets and Animals',
+    '17' => 'Sports',
+    '18' => 'Short movies',
+    '19' => 'Travel',
+    '20' => 'Games',
+    '21' => 'Podcast',
+    '22' => 'People',
+    '23' => 'Comedy',
+    '24' => 'Entertainment',
+    '25' => 'News and Politics',
+    '26' => 'DIY',
+    '27' => 'Education',
+    '28' => 'Science',
+    '30' => 'Moview',
+    '31' => 'Comics',
+    '32' => 'Action',
+    '33' => 'Classic',
+    '34' => 'Comedy',
+    '35' => 'Documentary',
+    '36' => 'Drama',
+    '37' => 'Family',
+    '38' => 'Foreign',
+    '39' => 'Horror',
+    '40' => 'Sci-FI',
+    '41' => 'Thriller',
+    '42' => 'Short film',
+    '43' => 'Shows',
+    '44' => 'Trailer'
        );
      if($id=='all'){
      return $data;    
@@ -135,17 +135,17 @@ function format_date($time){
     $t=strtotime($time);
     $t=time()-$t;
     $f=array(
-    '31536000'=>'年',
-    '2592000'=>'个月',
-    '604800'=>'周',
-    '86400'=>'天',
-    '3600'=>'小时',
-    '60'=>'分钟',
-    '1'=>'秒'
+    '31536000'=>'years',
+    '2592000'=>'months',
+    '604800'=>'weeks',
+    '86400'=>'days',
+    '3600'=>'hours',
+    '60'=>'minutes',
+    '1'=>'seconds'
     );
     foreach ($f as $k=>$v)    {
         if (0 !=$c=floor($t/(int)$k)) {
-            return $c.$v.'前';
+            return $c.$v.'ago';
         }
     }
 }
@@ -230,9 +230,9 @@ function video_down($v,$name){
 $yt = new YouTubeDownloader();
 $links = $yt->getDownloadLinks("https://www.youtube.com/watch?v=$v");
 echo '<table class="table table-hover"><thead><tr>
-      <th>格式</th>
-      <th>类型</th>
-      <th>下载</th>
+      <th>Format</th>
+      <th>Type</th>
+      <th>Download</th>
     </tr>
   </thead>';
 foreach ($links as $value) {
@@ -242,7 +242,7 @@ echo ' <tbody>
       
       <td>'.$videotype[$value['format']][0].'</td>
       <td>'.$videotype[$value['format']][1].'</td>
-      <td><a href="./downvideo.php?v='.$v.'&quality='.$value['format'].'&name='.$name.'&format='.$videotype[$value['format']][2].'" target="_blank" class="btn btn-outline-success btn-sm">下载</a></td>
+      <td><a href="./downvideo.php?v='.$v.'&quality='.$value['format'].'&name='.$name.'&format='.$videotype[$value['format']][2].'" target="_blank" class="btn btn-outline-success btn-sm">Download</a></td>
     </tr></tbody>';
     } 
     echo '</table>';
@@ -282,43 +282,43 @@ function Hislist($str,$apikey){
 //获取频道所属国家
 $CountryID=array(
     'DZ' => '阿尔及利亚',
-    'AR' => '阿根廷',
-    'AE' => '阿拉伯联合酋长国',
+    'AR' => 'Argentina',
+    'AE' => 'UAE',
     'OM' => '阿曼',
     'AZ' => '阿塞拜疆',
-    'EG' => '埃及',
-    'IE' => '爱尔兰',
+    'EG' => 'Egypt',
+    'IE' => 'Ireland',
     'EE' => '爱沙尼亚',
     'AT' => '奥地利',
     'AU' => '澳大利亚',
     'PK' => '巴基斯坦',
     'BH' => '巴林',
-    'BR' => '巴西',
+    'BR' => 'Brazil',
     'BY' => '白俄罗斯',
     'BG' => '保加利亚',
     'BE' => '比利时',
-    'IS' => '冰岛',
+    'IS' => 'Iceland',
     'PR' => '波多黎各',
-    'PL' => '波兰',
+    'PL' => 'Poland',
     'BA' => '波斯尼亚和黑塞哥维那',
-    'DK' => '丹麦',
-    'DE' => '德国',
+    'DK' => 'Denmark',
+    'DE' => 'Russia',
     'RU' => '俄罗斯',
-    'FR' => '法国',
+    'FR' => 'France',
     'PH' => '菲律宾',
     'FI' => '芬兰',
     'CO' => '哥伦比亚',
     'GE' => '格鲁吉亚共和国',
     'KZ' => '哈萨克斯坦',
-    'KR' => '韩国',
+    'KR' => 'South Korea',
     'NL' => '荷兰',
     'ME' => '黑山共和国',
-    'CA' => '加拿大',
-    'CN' => '中国',
+    'CA' => 'Canada',
+    'CN' => 'China',
     'GH' => '加纳',
     'CZ' => '捷克共和国',
     'ZW' => '津巴布韦',
-    'QA' => '卡塔尔',
+    'QA' => 'Qatar',
     'KW' => '科威特',
     'HR' => '克罗地亚',
     'KE' => '肯尼亚',
@@ -330,16 +330,16 @@ $CountryID=array(
     'RO' => '罗马尼亚',
     'MY' => '马来西亚',
     'MK' => '马其顿',
-    'US' => '美国',
+    'US' => 'US',
     'PE' => '秘鲁',
     'MA' => '摩洛哥',
     'MX' => '墨西哥',
-    'ZA' => '南非',
-    'NP' => '尼泊尔',
+    'ZA' => 'South Africa',
+    'NP' => 'Nepal',
     'NG' => '尼日利亚',
     'NO' => '挪威',
     'PT' => '葡萄牙',
-    'JP' => '日本',
+    'JP' => 'japan',
     'SE' => '瑞典',
     'CH' => '瑞士',
     'RS' => '塞尔维亚',
@@ -348,30 +348,30 @@ $CountryID=array(
     'LK' => '斯里兰卡',
     'SK' => '斯洛伐克',
     'SI' => '斯洛文尼亚',
-    'TW' => '台湾',
-    'TH' => '泰国',
+    'TW' => 'Taiwan',
+    'TH' => 'Thailand',
     'TZ' => '坦桑尼亚',
     'TN' => '突尼斯',
-    'TR' => '土耳其',
-    'UG' => '乌干达',
-    'UA' => '乌克兰',
-    'ES' => '西班牙',
+    'TR' => 'Turkey',
+    'UG' => 'Uganda',
+    'UA' => 'Ukraine',
+    'ES' => 'Spain',
     'GR' => '希腊',
-    'HK' => '香港',
-    'SG' => '新加坡',
-    'NZ' => '新西兰',
+    'HK' => 'Hing Kong',
+    'SG' => 'Singapore',
+    'NZ' => 'New Zealand',
     'HU' => '匈牙利',
     'JM' => '牙买加',
     'YE' => '也门',
-    'IQ' => '伊拉克',
-    'IL' => '以色列',
-    'IT' => '意大利',
-    'IN' => '印度',
+    'IQ' => 'Iraq',
+    'IL' => 'Israel',
+    'IT' => 'Italy',
+    'IN' => 'India',
     'ID' => '印尼',
-    'GB' => '英国',
-    'JO' => '约旦',
-    'VN' => '越南',
-    'CL' => '智利',
+    'GB' => 'UK',
+    'JO' => 'Jordan',
+    'VN' => 'Vietnam',
+    'CL' => 'Chile',
     );
 function get_country($c){
     global $CountryID;
@@ -408,31 +408,31 @@ function strdecode($string,$key='09KxDsIIe|+]8Fo{YP<l+3!y#>a$;^PzFpsxS9&d;!l;~M>
 }
 
 //分享功能
-function shareit($id,$title='免翻墙Youtube镜像'){
+function shareit($id,$title='YouTube Proxy'){
     $pic=ROOT_PART.'/thumbnail.php?vid='.$id;
     $url=ROOT_PART.'watch-'.$id.'.html';
     $title=str_replace('&','||',$title);
     $title=str_replace('"',' ',$title);
      $title=str_replace("'",' ',$title);
-    $des='【免翻墙Youtube镜像】我正在通过这个网站看《'.$title.'》不用翻墙看全球视频，手机电脑都能看，快来试试吧！';
+    $des='YouTube Proxy';
     return "<div id='share'>
   <a class='icoqz' href='https://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=".$url."&desc=".$des."&title=".$titlel."
-&pics=".$pic."' target='blank' title='分享到QQ空间'><i class='iconfont icon-qqkongjian icofontsize'></i></a>
+&pics=".$pic."' target='blank' title='Share to Qzone'><i class='iconfont icon-qqkongjian icofontsize'></i></a>
 
-  <a class='icotb' href='http://tieba.baidu.com/f/commit/share/openShareApi?title=".$title."&url=".$url."&to=tieba&type=text&relateUid=&pic=".$pic."&key=&sign=on&desc=&comment=".$title."' target='blank' title='分享到贴吧'><i class='iconfont icon-40 icofontsize'></i></a>
+  <a class='icotb' href='http://tieba.baidu.com/f/commit/share/openShareApi?title=".$title."&url=".$url."&to=tieba&type=text&relateUid=&pic=".$pic."&key=&sign=on&desc=&comment=".$title."' target='blank' title='Share to tieba'><i class='iconfont icon-40 icofontsize'></i></a>
 
-  <a class='icowb' href='http://service.weibo.com/share/share.php?url=".$url."&title=".$des."&pic=".$pic."&sudaref=".$title."' target='blank' title='分享到微博'><i class='iconfont icon-weibo icofontsize'></i></a>
+  <a class='icowb' href='http://service.weibo.com/share/share.php?url=".$url."&title=".$des."&pic=".$pic."&sudaref=".$title."' target='blank' title='Share to Weibo'><i class='iconfont icon-weibo icofontsize'></i></a>
 
-  <a class='icobi' href='https://member.bilibili.com/v/#/text-edit' target='blank' title='分享到哔哩哔哩'><i class='iconfont icon-bilibili icofontsize'></i></a>
+  <a class='icobi' href='https://member.bilibili.com/v/#/text-edit' target='blank' title='Share to Bilibili'><i class='iconfont icon-bilibili icofontsize'></i></a>
 
-  <a class='icowx' href='http://api.addthis.com/oexchange/0.8/forward/wechat/offer?url=".ROOT_PART."watch.php?v=".$id."' target='blank' title='分享到微信' ><i class='iconfont icon-weixin icofontsize'></i></a>
+  <a class='icowx' href='http://api.addthis.com/oexchange/0.8/forward/wechat/offer?url=".ROOT_PART."watch.php?v=".$id."' target='blank' title='Share to Wechat' ><i class='iconfont icon-weixin icofontsize'></i></a>
 </div>
  <div class='form-group'><div class='d-inline-block h6 pt-3 col-12'>
-    分享代码：
+    Share code:
  </div>
     <textarea style='resize:none;height: auto' class='form-control d-inline align-middle col-12 icoys icontext' id='inputs' type='text' rows='5' placeholder='Default input'><iframe height=498 width=510 src=&quot;".ROOT_PART."embed/?v=".$id.";&quot; frameborder=0 &quot;allowfullscreen&quot;></iframe></textarea>
     
-    <button type='submit' class='btn btn-primary align-middle col-12 mt-2' onclick='copytext1()'>复制</button></div>";
+    <button type='submit' class='btn btn-primary align-middle col-12 mt-2' onclick='copytext1()'>Copy</button></div>";
     
 }
 //
